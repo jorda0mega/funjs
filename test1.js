53bd88c950524de9d461693cccdf15643ca97791
jorda0mega <jorda0mega@gmail.com>
2013-11-12 11:24:10 -0500

    remove test files obsolete

--- test1.js
+++
@@ -1,8 +0,0 @@
-global1 = "this is a test for the first global";
-var global2 = "this is a test for the second global";
-
-function helloworld(){
-  global3 = "3rd global";
-  var global4 = "4th global";
-  alert();
-}
